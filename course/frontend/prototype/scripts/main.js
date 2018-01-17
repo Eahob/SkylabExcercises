12821e2c54c46cb5e935356f78ce16dfb6a4ed1d
to_do <to_do>
2018-01-17 17:45:12 +0100

    more things

---
+++ course/frontend/prototype/scripts/main.js
@@ -0,0 +1,20 @@
+/*
+Person
+
+a) Write a constructor function called Person and that has the following arguments:
+
+first-name          //string
+last-name           //string
+age                 //number
+gender              //string
+interests           //array
+
+*/
+
+function Person(firstName, lastName, age, gender, interests) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.gender = gender;
+    this.interests;
+}
