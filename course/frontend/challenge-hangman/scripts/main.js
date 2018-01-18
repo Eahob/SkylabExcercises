04e00d08f99bec9e78c59ac948ffd8d6a5c93102
to_do <to_do>
2018-01-18 16:55:54 +0100

    add more exec, hangman

---
+++ course/frontend/challenge-hangman/scripts/main.js
@@ -0,0 +1,36 @@
+/* Recreate the game of Hangman */
+
+function Hangman (word,attemps) {
+    var wordArr = word.toLowerCase().split('');
+    var board = [];
+    var N = word.length;
+    var counter = 0;
+    for (var i = 0; i < N; i++) {
+        board[i] = '_';
+    }
+    this.try = function (response) {
+        var str = response.toLowerCase();
+        function gameOver () {return "Game Over";}
+        if (str.length > 1 && str === word) {
+            return "Very well! You Guessed!";
+        } else if (str.length === 1 ){
+            if ( --attemps > 0) {
+                for (var i = 0; i < N; i++) {
+                    if (str === wordArr[i]) {
+                        board[i] = wordArr[i];
+                        delete wordArr[i];
+                        counter++;
+                    }
+                }
+                //if (N === counter) {}
+                return attemps + ' ' + board.join(' ') + (N === counter ? '- You won!':'');
+            } else {
+                return gameOver();
+            }
+        } else {
+            return gameOver();
+        }
+    }
+}
+
+var game = new Hangman ('hello',5);
