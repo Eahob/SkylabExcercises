f6e5a121d2f3aa6ffda4361a407ad32f9b576d41
Eahob <lerbfae@gmail.com>
2018-02-28 19:00:59 +0100

    update

--- course/backend/task-full/task-api/index.js
+++ course/backend/task-full/task-api/index.js
@@ -9,10 +9,12 @@ let ids = 1
 
 app.post('/api/tasks', jsonBodyParser, (req, res) => {
     const { text } = req.body
-    if (!text < 0) return res.json('Invalid task registration')
-    if (tasks.length !== 0) { tasks[tasks.length - 1].id = ids++ }
-    tasks.push({ id: ids, text, done: false })
-    res.json('PUT ok')
+    if (text.length) {
+        tasks.push({ id: ids++, text, done: false })
+        res.json('Task added to list')
+    } else {
+        res.json('Invalid task registration')
+    }
 })
 
 app.get('/api/tasks', (req, res) => {
@@ -20,38 +22,30 @@ app.get('/api/tasks', (req, res) => {
 })
 
 app.get('/api/tasks/done', (req, res) => {
-    const tasksDone = tasks.filter(task => {
-        return task.done === true
-    })
+    const tasksDone = tasks.filter(task => task.done)
     res.json(tasksDone.map(({ text }) => ({ text })))
 })
 
 app.get('/api/tasks/todo', (req, res) => {
-    const todo = tasks.filter(task => {
-        return task.done === false
-    })
+    const todo = tasks.filter(task => !task.done)
     res.json(todo.map(({ text }) => ({ text })))
 })
 
 app.put('/api/tasks/:id', (req, res) => {
     const { id } = req.params
-    const find = tasks.find(task => {
-        return task.id == id
-    })
+    const find = tasks.find(task => task.id == id)
     find.done = true
-    res.send('OK')
+    res.send('Task marked as done')
 })
 
 app.delete('/api/tasks/:id', (req, res) => {
     const { id } = req.params
-    const index = tasks.findIndex(task => {
-        return task.id == id
-    })
+    const index = tasks.findIndex(task => task.id == id)
     if (index < 0) {
         res.json({ error: "No task to delete" })
     } else {
         tasks.splice(index, 1)
-        res.send('Deletion OK')
+        res.send('Task succesfully deleted')
     }
 })
 
@@ -63,11 +57,9 @@ app.delete('/api/tasks', (req, res) => {
 app.patch('/api/tasks/:id', jsonBodyParser, (req, res) => {
     const id = req.params.id
     const { text } = req.body
-    const index = tasks.findIndex(task => {
-        return task.id == id
-    })
+    const index = tasks.findIndex(task => task.id == id)
     if (index < 0) {
-        res.json({ error: "No task to patch" })
+        res.json({ error: "No task to change" })
     } else {
         tasks[index].text = text
         res.json(tasks[index])
